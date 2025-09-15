a7ac638df12b9ddbfc10da5c699d5f4929fb0102
Max Ratushniy <st140678@student.spbu.ru>
2025-09-15 15:03:24 +0300

    Fix integer_div

--- 09.25/integer_div.c
+++ 09.25/integer_div.c
@@ -1,28 +1,20 @@
 #include <stdio.h>
-
-// Return the absolute value of x
-int abs(int x) {
-    return -x ? x < 0 : x;
-}
+#include <stdlib.h>
 
 // Return -1 if x is negative and 1 otherwise
 int sign(int x) {
-    if (x < 0) {
-        return -1;
-    } else {
-        return 1;
-    }
+    return (x < 1) ? -1 : 1;
 }
 
 // Find the integer part of a divided by b
 int div_int(int a, int b) {
     int count = 0;
-    int _a = abs(a);
-    int _b = abs(b);
+    int abs_a = abs(a);
+    int abs_b = abs(b);
 
-    while (_a >= _b) {
+    while (abs_a >= abs_b) {
         count += 1;
-        _a -= _b;
+        abs_a -= abs_b;
     }
 
     return count * sign(a) * sign(b);
@@ -30,13 +22,11 @@ int div_int(int a, int b) {
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        puts("fuck1");
         return -1;
     }
 
     int a, b;
     if (sscanf(argv[1], "%d", &a) * sscanf(argv[2], "%d", &b) != 1) {
-        puts("fuck2");
         return -1;
     }
 
