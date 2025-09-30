dea97e75ee7e85c540b3bef8a0bdbf0795d66ffd
Max Ratushniy <st140678@student.spbu.ru>
2025-09-30 19:26:35 +0300

    Implement parenthesis balance

---
+++ 09.25/parens_balance.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include <string.h>
+#include <stddef.h>
+
+// Check whether parenthesis are balanced in the provided string literal.
+bool balanced(const char* str) {
+    size_t counter = 0;
+
+    for (ptrdiff_t i = 0; i < strlen(str); i++) {
+        char c = str[i];
+
+        if (c == '(') {
+            counter++;
+        } else if (c == ')') {
+            if (counter == 0) {
+                return false;
+            }
+
+            counter--;
+        }
+    }
+
+    return counter == 0;
+}
